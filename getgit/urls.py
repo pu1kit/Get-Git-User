b3ec4caa3e10644421680779b45fb8229859f79a
pu1kit <=>
2015-12-13 23:01:06 +0530

    completed

---
+++ getgit/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'getgit.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^app/', include('app.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
